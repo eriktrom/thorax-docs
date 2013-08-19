03de3c62c48bcde0d3e31bb0e98d8a143f835164
colinmegill <colinmegill@gmail.com>
2013-08-19 14:55:18 -0700

    pulling out file splitting for dev

--- grunt/process-readme.js
+++ grunt/process-readme.js
@@ -24,28 +24,5 @@ module.exports = function(grunt) {
 
   });
 
-  function processReadme(text, done) {
-    var segments = text.split(/\n\#\s/m).map(function(segment, i) {
-      if (i !== 0) {
-        segment = '# ' + segment;
-      }
-      return segment;
-    });
-
-    // segments SHOULD contain:
-    // [0] thorax
-    // [1] quick start
-    // [2] features
-    // [3] getting started
-    // [4] API Reference
-    // [5] Error Codes
-
-    grunt.file.write('src/content/quick-start.md', segments[1]);
-    grunt.file.write('src/content/features.md', segments[2].replace(/^.+/, '')); // will remove first line only
-    grunt.file.write('src/content/api.md', segments[3]);
-
-    
-    done(true);
-  }
 
 };
